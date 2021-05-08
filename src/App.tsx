3818c5b1aae0e2f7ef04ab939a1d81cb35443f7b
tiagomatos8 <tcm.matos@gmail.com>
2021-05-08 23:21:51 +1000

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,8 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import Link from '@material-ui/core/Link';
-import ProTip from './ProTip';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+import ProTip from './components/molecules/ProTip';
+import Copyright from './components/atoms/Copyright';
 
 export default function App() {
   return (
