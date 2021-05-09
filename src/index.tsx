20c55a5588255f0d39f80b232985708963c5e4a5
tiagomatos8 <tcm.matos@gmail.com>
2021-05-09 13:58:32 +1000

    WIP react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import App from './App';
 import theme from './theme';
 
